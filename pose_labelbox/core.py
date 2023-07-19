18437e2332d483b3fc2f0928a99b620053c9851d
Theodore Quinn <tcquinn@tcquinn.org>
2023-07-19 14:33:42 -0500

    Implement function to generate target_video_starts

--- pose_labelbox/core.py
+++ pose_labelbox/core.py
@@ -1,1 +1,27 @@
-# START CODE HERE
+import pandas as pd
+import datetime
+
+def generate_target_video_starts(
+    start,
+    end,
+    video_duration=datetime.timedelta(seconds=10),
+    video_duration_string='10S'
+):
+    start_utc = convert_to_datetime_utc(start)
+    end_utc = convert_to_datetime_utc(end)
+    first_video_start_utc = pd.Timestamp(start_utc).floor(video_duration)
+    last_video_start_utc = pd.Timestamp(end_utc).ceil(video_duration_string) - video_duration
+    target_video_starts = (
+        pd.date_range(
+            start=first_video_start_utc,
+            end=last_video_start_utc,
+            freq=video_duration
+        )
+        .to_pydatetime()
+        .tolist()
+    )
+    return target_video_starts
+
+
+def convert_to_datetime_utc(datetime_object):
+    return pd.to_datetime(datetime_object, utc=True).to_pydatetime()
