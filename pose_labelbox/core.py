7ddd3cb5f22d6b0d1ab97bc856819f4cea1834b5
Theodore Quinn <tcquinn@tcquinn.org>
2023-07-19 14:50:01 -0500

    Add function to generate target camera IDs

--- pose_labelbox/core.py
+++ pose_labelbox/core.py
@@ -1,3 +1,4 @@
+import honeycomb_io
 import pandas as pd
 import datetime
 
@@ -22,6 +23,42 @@ def generate_target_video_starts(
     )
     return target_video_starts
 
+def generate_target_camera_ids(
+    start,
+    end,
+    environment_id=None,
+    environment_name=None,
+    device_ids=None,
+    part_numbers=None,
+    serial_numbers=None,
+    names=None,
+    client=None,
+    uri=None,
+    token_uri=None,
+    audience=None,
+    client_id=None,
+    client_secret=None      
+):
+    target_camera_ids = honeycomb_io.fetch_device_ids(
+        device_types=honeycomb_io.DEFAULT_CAMERA_DEVICE_TYPES,
+        device_ids=device_ids,
+        part_numbers=part_numbers,
+        serial_numbers=serial_numbers,
+        tag_ids=None,
+        names=names,
+        environment_id=environment_id,
+        environment_name=environment_name,
+        start=start,
+        end=end,
+        chunk_size=100,
+        client=client,
+        uri=uri,
+        token_uri=token_uri,
+        audience=audience,
+        client_id=client_id,
+        client_secret=client_secret
+    )
+    return target_camera_ids
 
 def convert_to_datetime_utc(datetime_object):
     return pd.to_datetime(datetime_object, utc=True).to_pydatetime()
